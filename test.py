14033d9b850c31ad64ffb6bc6255dc388915e2bd
Laidson Paes <62618287+Laidson@users.noreply.github.com>
2020-04-16 20:31:03 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,32 @@
+from pygame_functions import *
+
+
+screenSize(400, 400)
+
+def user():
+
+    showLabel(makeLabel('SSSSSSS', 40, 10, 10, 'blue'))
+
+    box = makeTextBox(20, 80, 100, 0, "hhhhhhhh",)
+    showTextBox(box)
+
+    textBoxInput(box)
+
+def get_user():
+    usuario = user()
+
+    return usuario
+
+
+while True:
+    nome = user()#int_user_name
+
+
+
+    break
+
+
+
+
+
+
